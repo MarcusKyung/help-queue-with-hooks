3301e3e4ff18f3a6d981a71c1ced1610e69e88bd
MarcusKyung <Kyungmj@gmail.com>
2023-07-11 14:53:20 -0700

    add carousel and styling test

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,31 +1,69 @@
 import React from "react";
 import Ticket from "./Ticket";
 import PropTypes from "prop-types";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-function TicketList(props){
-
+function TicketList(props) {
   return (
     <React.Fragment>
-      <hr/>
-      {props.ticketList.map((ticket) =>
-        <Ticket 
-          whenTicketClicked={props.onTicketSelection}
-          names={ticket.names}
-          location={ticket.location}
-          issue={ticket.issue}
-          id={ticket.id}
-          key={ticket.id}
-          author={ticket.author}
-          formattedWaitTime={ticket.formattedWaitTime}/>
-      )}
+      <hr />
+      <Carousel showArrows={true} infiniteLoop={false}>
+        {props.ticketList.map((ticket) => (
+          <Ticket
+            whenTicketClicked={props.onTicketSelection}
+            names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue}
+            id={ticket.id}
+            key={ticket.id}
+            author={ticket.author}
+            formattedWaitTime={ticket.formattedWaitTime}
+          />
+        ))}
+      </Carousel>
     </React.Fragment>
   );
 }
 
 TicketList.propTypes = {
   ticketList: PropTypes.array,
-  onTicketSelection: PropTypes.func
+  onTicketSelection: PropTypes.func,
 };
 
 export default TicketList;
+
+
+
+
+// import React from "react";
+// import Ticket from "./Ticket";
+// import PropTypes from "prop-types";
+
+// function TicketList(props){
+
+//   return (
+//     <React.Fragment>
+//       <hr/>
+//       {props.ticketList.map((ticket) =>
+//         <Ticket 
+//           whenTicketClicked={props.onTicketSelection}
+//           names={ticket.names}
+//           location={ticket.location}
+//           issue={ticket.issue}
+//           id={ticket.id}
+//           key={ticket.id}
+//           author={ticket.author}
+//           formattedWaitTime={ticket.formattedWaitTime}/>
+//       )}
+//     </React.Fragment>
+//   );
+// }
+
+// TicketList.propTypes = {
+//   ticketList: PropTypes.array,
+//   onTicketSelection: PropTypes.func
+// };
+
+// export default TicketList;
 
